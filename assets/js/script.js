5a078f91a432b6020e47a8fe95632a168946e283
dualsgo <73504705+dualsgo@users.noreply.github.com>
2022-12-10 20:48:46 -0300

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,113 +1,118 @@
 //Código padrão para interatividade do menu hamurguer com SVG
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+  }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //Código modificado apenas adaptando a function para linkar as outras páginas já que o SVG está em outro diretório apresentou bug
 
 function menuShowPages() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/assets/img/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "/assets/img/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "/assets/img/close_white_36dp.svg";
+  }
 }
 
 /////////////////////////////////////////////////
 
-
 function limpa_formulário_cep() {
-    //Limpa valores do formulário de cep.
-    document.getElementById('rua').value = ("");
-    document.getElementById('bairro').value = ("");
-    document.getElementById('cidade').value = ("");
-    document.getElementById('uf').value = ("");
-    document.getElementById('ibge').value = ("");
-  }
+  //Limpa valores do formulário de cep.
+  document.getElementById("rua").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("uf").value = "";
+  document.getElementById("ibge").value = "";
+}
 
-  function meu_callback(conteudo) {
-    if (!("erro" in conteudo)) {
-      //Atualiza os campos com os valores.
-      document.getElementById('rua').value = (conteudo.logradouro);
-      document.getElementById('bairro').value = (conteudo.bairro);
-      document.getElementById('cidade').value = (conteudo.localidade);
-      document.getElementById('uf').value = (conteudo.uf);
-      document.getElementById('ibge').value = (conteudo.ibge);
+function meu_callback(conteudo) {
+  if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById("rua").value = conteudo.logradouro;
+    document.getElementById("bairro").value = conteudo.bairro;
+    document.getElementById("cidade").value = conteudo.localidade;
+    document.getElementById("uf").value = conteudo.uf;
+    document.getElementById("ibge").value = conteudo.ibge;
+  } //end if.
+  else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+  }
+}
+
+function pesquisacep(valor) {
+  //Nova letiável "cep" somente com dígitos.
+  let cep = valor.replace(/\D/g, "");
+
+  //Verifica se campo cep possui valor informado.
+  if (cep != "") {
+    //Expressão regular para validar o CEP.
+    let validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if (validacep.test(cep)) {
+      //Preenche os campos com "..." enquanto consulta webservice.
+      document.getElementById("rua").value = "...";
+      document.getElementById("bairro").value = "...";
+      document.getElementById("cidade").value = "...";
+      document.getElementById("uf").value = "...";
+      document.getElementById("ibge").value = "...";
+
+      //Cria um elemento javascript.
+      let script = document.createElement("script");
+
+      //Sincroniza com o callback.
+      script.src =
+        "https://viacep.com.br/ws/" + cep + "/json/?callback=meu_callback";
+
+      //Insere script no documento e carrega o conteúdo.
+      document.body.appendChild(script);
     } //end if.
     else {
-      //CEP não Encontrado.
+      //cep é inválido.
       limpa_formulário_cep();
-      alert("CEP não encontrado.");
+      alert("Formato de CEP inválido.");
+    }
+  } //end if.
+  else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+  }
+}
+
+function check() {
+  let checkBox = document.getElementById("checbox");
+  let text1 = document.getElementsByClassName("text1");
+  let text2 = document.getElementsByClassName("text2");
+
+  for (let i = 0; i < text1.length; i++) {
+    if (checkBox.checked == true) {
+      text1[i].style.display = "block";
+      text2[i].style.display = "none";
+    } else if (checkBox.checked == false) {
+      text1[i].style.display = "none";
+      text2[i].style.display = "block";
     }
   }
+}
+check();
 
-  function pesquisacep(valor) {
+function mensagemLista() {
+  alert("Entraremos em contato!");
+}
 
-    //Nova letiável "cep" somente com dígitos.
-    let cep = valor.replace(/\D/g, '');
-
-    //Verifica se campo cep possui valor informado.
-    if (cep != "") {
-
-      //Expressão regular para validar o CEP.
-      let validacep = /^[0-9]{8}$/;
-
-      //Valida o formato do CEP.
-      if (validacep.test(cep)) {
-
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('rua').value = "...";
-        document.getElementById('bairro').value = "...";
-        document.getElementById('cidade').value = "...";
-        document.getElementById('uf').value = "...";
-        document.getElementById('ibge').value = "...";
-
-        //Cria um elemento javascript.
-        let script = document.createElement('script');
-
-        //Sincroniza com o callback.
-        script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
-
-        //Insere script no documento e carrega o conteúdo.
-        document.body.appendChild(script);
-
-      } //end if.
-      else {
-        //cep é inválido.
-        limpa_formulário_cep();
-        alert("Formato de CEP inválido.");
-      }
-    } //end if.
-    else {
-      //cep sem valor, limpa formulário.
-      limpa_formulário_cep();
-    }
-  };
-
-  function check() {
-    let checkBox = document.getElementById("checbox");
-    let text1 = document.getElementsByClassName("text1");
-    let text2 = document.getElementsByClassName("text2");
-  
-    for (let i = 0; i < text1.length; i++) {
-      if (checkBox.checked == true) {
-        text1[i].style.display = "block";
-        text2[i].style.display = "none";
-      } else if (checkBox.checked == false) {
-        text1[i].style.display = "none";
-        text2[i].style.display = "block";
-      }
-    }
-  }
-  check();
-  
+function mensagemRecupera() {
+  alert(
+    "Enviamos as instruções para recuperação de senha para o emial cadastrado!"
+  );
+}
